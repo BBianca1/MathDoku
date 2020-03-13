30c585e8a1d3bdeffbd75e6c3d5d6560367cde5e
cbb1u19 <cbb1u19@soton.ac.uk>
2020-03-13 23:50:38 +0000

    cages?

---
+++ Cage.java
@@ -0,0 +1,12 @@
+import javafx.stage.Stage;
+
+public class Cage extends javafx.application.Application{
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+	
+}
